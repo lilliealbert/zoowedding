71bc122fdc821cb389ba42a50f222dfa6034a808
Lillie Chilen <lillie.chilen@gmail.com>
2014-03-16 16:21:08 -0700

    Add basic auth to admin pages

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,6 @@
 class AdminController < ApplicationController
+  http_basic_authenticate_with name: ENV["ADMIN_USERNAME"], password: ENV["ADMIN_PASSWORD"]
+
   def index
     @guests = Guest.all
     @rsvps = Rsvp.all
