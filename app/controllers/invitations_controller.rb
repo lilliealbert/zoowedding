e33d2a2a131a186d404d58ca974deb713fdebb7f
Lillie Chilen <lillie.chilen@gmail.com>
2014-03-30 22:14:33 -0700

    Show friendly thanks flash messages on RSVP

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -8,7 +8,7 @@ class InvitationsController < ApplicationController
   def update
     @invitation = Invitation.find(params[:id])
     if @invitation.update_attributes(invitation_params)
-      flash[:success] = "Thanks for your RSVP!"
+      flash[:success] = thanks_message(params[:commit])
     else
       flash[:warning] = @invitation.errors.full_messages
     end
@@ -24,5 +24,15 @@ class InvitationsController < ApplicationController
 
   def load_invitation
     @invitation = Invitation.find_by_external_id(params[:external_id])
+  end
+
+  def thanks_message(button_text)
+    if button_text == "Yes! See you soon!"
+      "Woo hoo! We can't wait to see you!"
+    elsif button_text == "Shoot, we can't make it at all."
+      "Aw, bummer you can't make it. Thanks for RSVPing, though!"
+    else
+      "Thanks for RSVPing!"
+    end
   end
 end
