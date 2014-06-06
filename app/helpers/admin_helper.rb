65b8b3e05b589057e5db0bbc5d819637159f2497
Lillie Chilen <lillie.chilen@gmail.com>
2014-06-05 21:44:38 -0700

    Show guest counts instead of invitation counts on admin page

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -2,4 +2,19 @@ module AdminHelper
   def event_rsvp(invitation, event_title)
     invitation.rsvps.where(event: Event.find_by_title(event_title)).first.attending
   end
+
+  def total_event_guest_count(event)
+    event = Event.where(title: event).first
+    Rsvp.where(event: event).map(&:invitation).map(&:guests).map(&:length).sum
+  end
+
+  def yes_event_guest_count(event)
+    event = Event.where(title: event).first
+    Rsvp.yeses.where(event: event).map(&:invitation).map(&:guests).map(&:length).sum
+  end
+
+  def nos_event_guest_count(event)
+    event = Event.where(title: event).first
+    Rsvp.nos.where(event: event).map(&:invitation).map(&:guests).map(&:length).sum
+  end
 end
--- spec/features/admin_spec.rb
+++ spec/features/admin_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe "doing admin things" do
   before do
     page.driver.browser.authorize ENV['ADMIN_USERNAME'], ENV['ADMIN_PASSWORD']
+    Event::TITLES.each { |title| Event.create(title: title) }
   end
 
   it "allows the admin to send emails" do
